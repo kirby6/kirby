5cd4416464db49226f20edea1a4b3d06ed1e8cbf
dvir42 <dviros7@gmail.com>
2019-01-31 08:00:48 +0200

    replaced werkzeug with gevent

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from gevent.pywsgi import WSGIServer
+
 from kirby.core.config import BUILTINS_DIRECTORY, PLUGINS_DIRECTORY, SERVER_ADDRESS
 from kirby.core.private import loader as loader, router as router
 
@@ -6,9 +8,8 @@ if __name__ == '__main__':
     builtins.load_all()
     plugins = loader.Loader(PLUGINS_DIRECTORY)
     plugins.load_all()
-    router.app.run(**SERVER_ADDRESS)
-
-
+    print('Serving on: %s' % str(SERVER_ADDRESS))
+    WSGIServer((SERVER_ADDRESS['host'], SERVER_ADDRESS['port']), router.app).serve_forever()
 
 """
   <div *plugin="matrix"> asdasdasd </div>
