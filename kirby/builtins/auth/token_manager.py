8fb106253ff691fb481989a55baaa35c0b1ee072
dvir42 <dviros7@gmail.com>
2019-02-07 15:32:42 +0200

    fixed import bug

--- kirby/builtins/auth/token_manager.py
+++ kirby/builtins/auth/token_manager.py
@@ -1,13 +1,12 @@
-from flask import jsonify
 from flask_jwt_extended import JWTManager, create_access_token
 
-from kirby.core import web_api
 from kirby.core.db import collection as auth
 from kirby.core.private.router import app
-from kirby.core.config import config
+import kirby.core.config as config
 
 app.config['JWT_SECRET_KEY'] = auth.find_one()['secret']
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = config.JWT_ACCESS_TOKEN_EXPIRES or 1*60*60 # 1 hour as default
+app.config[
+    'JWT_ACCESS_TOKEN_EXPIRES'] = config.JWT_ACCESS_TOKEN_EXPIRES or 1 * 60 * 60  # 1 hour as default
 jwt = JWTManager(app)
 
 
@@ -20,6 +19,6 @@ def add_claims_to_access_token(user):
 
 
 def get_token(user=None):
-    if(not user):
+    if (not user):
         return {"error": "Corrupted user data"}
     return create_access_token(identity=user)
