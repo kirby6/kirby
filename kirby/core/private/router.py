809c9365ab503f6a6343996ac44e0f784662714d
moshew <Moshewiner13@gmail.com>
2019-02-09 15:45:23 +0200

    bugfix: trailing slashes on server routes ignored

--- kirby/core/private/router.py
+++ kirby/core/private/router.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 CORS(app, supports_credentials=True)
 
 @app.errorhandler(Exception)
