fdcfe2b16826bcb264b84345dd8e98be8341f378
moshew <Moshewiner13@gmail.com>
2019-02-17 19:02:50 +0200

    Added material icons for offline work

--- kirby-ui/src/app/pages/Assignments/assignments.component.ts
+++ kirby-ui/src/app/pages/Assignments/assignments.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { of as ObservableOf } from 'rxjs';
 import { Observable } from 'rxjs';
 import { NavigationItem } from 'src/app/components/navigation-list/interfaces';
+import { map } from 'rxjs/operators';
 
 @Component({
     templateUrl: './assignments.component.html',
@@ -26,34 +27,35 @@ export class AssignmentsPageComponent {
                 id: '0',
                 name: 'C#',
                 description: 'תרגול בC#',
-                isActive: false,
             },
             {
                 id: '1',
                 name: 'Web',
                 description: 'תרגול בWeb',
-                isActive: true,
             },
             {
                 id: '2',
                 name: 'Linux',
                 description: 'תרגול בLinux',
-                isActive: false,
             },
             {
                 id: '3',
                 name: 'Python',
                 description: 'תרגול בPython',
-                isActive: false,
             },
             {
                 id: '4',
                 name: 'Git',
                 description: 'תרגול בGit',
-                isActive: false,
             },
 
-        ]);
+        ])
+            .pipe(
+                map((assignments: NavigationItem[]) => {
+                    assignments.forEach(a => a.isActive = (a.id === this.assignmentId));
+                    return assignments;
+                })
+            );
     }
 
 } 
