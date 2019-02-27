6317984129e323d59963c82dd9fa47f83619e6ed
dvir42 <dviros7@gmail.com>
2019-02-27 14:24:31 +0200

    modules plugins

---
+++ plugins/modules/controller.py
@@ -0,0 +1,18 @@
+from bson import json_util
+
+from kirby.core.db import bson_to_json, collection as modules
+
+
+def get_all_modules():
+    return bson_to_json(modules.find())
+
+
+def get_module_by_name(name):
+    return bson_to_json(modules.find_one({'name': name}))
+
+
+def create_module(name, parent=None):
+    module_to_add = {'name': name}
+    if parent:
+        module_to_add['parent'] = parent
+    return json_util.dumps(modules.insert_one(module_to_add).inserted_id)
---
+++ plugins/modules/routes.py
@@ -0,0 +1,22 @@
+import json
+from flask import request
+
+from kirby.builtins.auth import required_roles
+from kirby.core import web_api
+from .controller import create_module, get_all_modules, get_module_by_name
+
+
+@web_api.route('/')
+def get_modules_route():
+    return json.dumps(get_all_modules())
+
+
+@web_api.route('/<string:module_name>')
+def get_module_by_name_route(module_name):
+    return json.dumps(get_module_by_name(module_name))
+
+
+@web_api.route('/', methods=['POST'])
+@required_roles('admin')
+def create_module_route():
+    return create_module(request.json['name'], request.json.get('parent'))
