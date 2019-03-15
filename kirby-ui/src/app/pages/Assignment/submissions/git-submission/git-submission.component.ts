b01adca78b7c53cb1a088536882ca96fa44d63d3
dvir42 <dviros7@gmail.com>
2019-03-15 19:47:45 +0200

    fixed css

--- kirby-ui/src/app/pages/Assignment/submissions/git-submission/git-submission.component.ts
+++ kirby-ui/src/app/pages/Assignment/submissions/git-submission/git-submission.component.ts
@@ -3,7 +3,7 @@ import { Assignment } from 'src/app/services/assignments/interfaces';
 
 @Component({
     selector: 'git-submission',
-    template: 'יש להגיש את התרגיל בקישור: <a href="{{submission.git.url}}">{{submission.git.url}}</a>',
+    template: 'יש להגיש את התרגיל בקישור:<br/><a href="{{submission.git.url}}">{{submission.git.url}}</a>',
     styles: [
         ':host { font-weight: 500; font-size: 16px; }',
         'a { color: rgba(255, 255, 255, 0.7); }',
